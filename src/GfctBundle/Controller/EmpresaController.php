c8c9e65427a2f724c59c8816daee4bbc32e75b7e
Daniel <daniel.espimarquez06@gmail.com>
2016-10-27 21:15:16 +0200

    subir formulario FCT

--- src/GfctBundle/Controller/EmpresaController.php
+++ src/GfctBundle/Controller/EmpresaController.php
@@ -3,15 +3,38 @@
 namespace GfctBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GfctBundle\Entity\Empresa;
+use GfctBundle\Form\EmpresaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class EmpresaController extends Controller
 {
+
   public function allAction()
   {
       $repository = $this->getDoctrine()->getRepository('GfctBundle:Empresa');
       // find *all* emp
       $emp = $repository->findAll();
       return $this->render('GfctBundle:Empresa:all.html.twig',array("empresa"=>$emp));
+  }
+
+  public function newAction(Request $request)
+  {
+    $empresa=new Empresa();
+    $form=$this->createForm(EmpresaType::class,$empresa);
+
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid()){
+      $empresa=$form->getData();
+
+      $em=$this->getDoctrine()->getManager();
+      $em->persist($empresa);
+      $em->flush();
+
+      //return $this->redirectToRoute('trask_success');
+    }
+
+    return $this->render('GfctBundle:Empresa:new.html.twig',array("form"=>$form->createView() ));
   }
 
 }
