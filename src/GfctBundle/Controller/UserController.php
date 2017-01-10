1e4516c5e91812aa0993c50388072dd335a5e094
Daniel <daniel.espimarquez06@gmail.com>
2017-01-10 10:55:49 +0100

    Subir Securizacion P06

---
+++ src/GfctBundle/Controller/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GfctBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GfctBundle\Entity\User;
+use GfctBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+
+  public function indexAction()
+  {
+      return $this->render('GfctBundle:Default:index.html.twig');
+  }
+
+  public function usuariosAction()
+  {
+      return $this->render('GfctBundle:Default:usuarios.html.twig');
+  }
+
+   public function registerAction(Request $request)
+   {
+       // 1) build the form
+       $user = new User();
+       $form = $this->createForm(UserType::class, $user);
+
+       // 2) handle the submit (will only happen on POST)
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid()) {
+
+           // 3) Encode the password (you could also do this via Doctrine listener)
+           $password = $this->get('security.password_encoder')
+               ->encodePassword($user, $user->getPlainPassword());
+           $user->setPassword($password);
+
+           // 4) save the User!
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($user);
+           $em->flush();
+
+           // ... do any other work - like sending them an email, etc
+           // maybe set a "flash" success message for the user
+
+           return new Response("Usuario registrado");
+       }
+
+       return $this->render('GfctBundle:Default:register.html.twig',array('form' => $form->createView())
+       );
+   }
+
+    public function loginAction(Request $request)
+    {
+      $authenticationUtils = $this->get('security.authentication_utils');
+
+      // get the login error if there is one
+      $error = $authenticationUtils->getLastAuthenticationError();
+
+      // last username entered by the user
+      $lastUsername = $authenticationUtils->getLastUsername();
+
+      return $this->render('GfctBundle:Default:login.html.twig', array(
+          'last_username' => $lastUsername,
+          'error'         => $error,
+      ));
+    }
+
+}
